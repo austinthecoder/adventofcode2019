a5daf45ac4ac6427d87716d9181f93c2318a247c
Austin Schneider <me@austinschneider.com>
2019-12-04 12:49:39 -0500

    better test output

--- spec/days_spec.rb
+++ spec/days_spec.rb
@@ -1,13 +1,17 @@
 RSpec.describe "days" do
   context "day 1" do
-    it "part 1 = 3477353" do
-      answer = app.calculate_total_fuel_required_v1(masses_file_path: "#{files_dir}/day_01_masses.txt")
-      expect(answer).to eq(3477353)
+    context "part 1" do
+      it "3477353" do
+        answer = app.calculate_total_fuel_required_v1(masses_file_path: "#{files_dir}/day_01_masses.txt")
+        expect(answer).to eq(3477353)
+      end
     end
 
-    it "part 2 = 5213146" do
-      answer = app.calculate_total_fuel_required(masses_file_path: "#{files_dir}/day_01_masses.txt")
-      expect(answer).to eq(5213146)
+    context "part 2" do
+      it "5213146" do
+        answer = app.calculate_total_fuel_required(masses_file_path: "#{files_dir}/day_01_masses.txt")
+        expect(answer).to eq(5213146)
+      end
     end
   end
 end
