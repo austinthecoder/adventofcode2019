75b0c8f273a08f56b82ca4c85bffedbc0e477099
Austin Schneider <me@austinschneider.com>
2019-12-04 23:34:01 -0500

    uneeded code

--- lib/adventofcode2019/mass_streamer.rb
+++ lib/adventofcode2019/mass_streamer.rb
@@ -3,9 +3,6 @@ module Adventofcode2019
   MassStreamer = Ivo.new do
     def stream(file_path:)
       File.foreach(file_path) do |line|
-        line = line.strip
-        next if line.empty?
-
         yield line.to_i
       end
     end
