5339feca1c64bcb436547dbb6318560b717a915f
Austin Schneider <me@austinschneider.com>
2019-12-06 20:30:40 -0500

    refactor

--- lib/adventofcode2019/intcode_program_state.rb
+++ lib/adventofcode2019/intcode_program_state.rb
@@ -2,29 +2,8 @@
 module Adventofcode2019
   IntcodeProgramState = Ivo.new(:codes, :instruction_pointer) do
     def next(on_input:, on_output:)
-      first_parameter_mode = (codes[instruction_pointer] / 100) % 10
-      second_parameter_mode = (codes[instruction_pointer] / 1_000) % 10
-
       case opcode
       when 1, 2
-        first_parameter = codes[instruction_pointer + 1]
-        first_value = case first_parameter_mode
-        when 0
-          codes[first_parameter]
-        when 1
-          first_parameter
-        end
-
-        second_parameter = codes[instruction_pointer + 2]
-        second_value = case second_parameter_mode
-        when 0
-          codes[second_parameter]
-        when 1
-          second_parameter
-        end
-
-        result_position = codes[instruction_pointer + 3]
-
         result = case opcode
         when 1
           first_value + second_value
@@ -32,11 +11,9 @@ module Adventofcode2019
           first_value * second_value
         end
 
-        next_codes = codes.merge(result_position => result)
-        instruction_size = 4
+        next_codes = codes.merge(third_parameter => result)
+        next_instruction_pointer = instruction_pointer + 4
       when 3
-        result_position = codes[instruction_pointer + 1]
-
         input = if on_input
           on_input.()
         else
@@ -46,29 +23,22 @@ module Adventofcode2019
 
         input = input.to_i
 
-        next_codes = codes.merge(result_position => input)
-        instruction_size = 2
+        next_codes = codes.merge(first_parameter => input)
+        next_instruction_pointer = instruction_pointer + 2
       when 4
-        output = case first_parameter_mode
-        when 0
-          codes[codes[instruction_pointer + 1]]
-        when 1
-          codes[instruction_pointer + 1]
-        end
-
         if on_output
-          on_output.(output)
+          on_output.(first_value)
         else
-          puts "output: #{output}"
+          puts "output: #{first_value}"
         end
 
         next_codes = codes
-        instruction_size = 2
+        next_instruction_pointer = instruction_pointer + 2
       else
         raise("Unexpected opcode: #{opcode}")
       end
 
-      self.class.new(next_codes, instruction_pointer + instruction_size)
+      self.class.new(next_codes, next_instruction_pointer)
     end
 
     def finished?
@@ -87,6 +57,40 @@ module Adventofcode2019
 
     def opcode
       codes[instruction_pointer] % 100
+    end
+
+    def first_parameter
+      codes[instruction_pointer + 1]
+    end
+
+    def second_parameter
+      codes[instruction_pointer + 2]
+    end
+
+    def third_parameter
+      codes[instruction_pointer + 3]
+    end
+
+    def first_value
+      first_parameter_mode = (codes[instruction_pointer] / 100) % 10
+
+      case first_parameter_mode
+      when 0
+        codes[first_parameter]
+      when 1
+        first_parameter
+      end
+    end
+
+    def second_value
+      second_parameter_mode = (codes[instruction_pointer] / 1_000) % 10
+
+      case second_parameter_mode
+      when 0
+        codes[second_parameter]
+      when 1
+        second_parameter
+      end
     end
   end
 end
